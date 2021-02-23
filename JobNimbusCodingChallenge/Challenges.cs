7e7f7ab745c65bbe941f2f0f7746023092b76616
christianbrotherson <25908849+christianbrotherson@users.noreply.github.com>
2021-02-22 18:30:39 -0700

    Completed sum of multiple of 3 and 5 within a given range and a check to see if brackets are closed correctly.

---
+++ JobNimbusCodingChallenge/Challenges.cs
@@ -0,0 +1,51 @@
+﻿namespace JobNimbusCodingChallenge
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Text;
+  using System.Linq;
+
+  /// <summary>
+  /// A class that contains the challenge problems.
+  /// </summary>
+  class Challenges
+  {
+    /// <summary>
+    /// Determines whether any brackets within the string are closed properly.
+    /// </summary>
+    /// <param name="str">The string to check.</param>
+    /// <returns>True if all brackets are closed.</returns>
+    public bool ClosedBrackets(string str)
+    {
+      var chars = str.ToCharArray().ToList();
+      if ((chars.FindAll(x => x == '{').Count != chars.FindAll(x => x == '}').Count) ||
+        (chars.IndexOf('{') > chars.IndexOf('}')))
+      {
+        return false;
+      }
+      else
+      {
+        int open = 0;
+        int closed = 0;
+        foreach (var c in chars)
+        {
+          if (closed > open) return false;
+          if (c == '{') open++;
+          else if (c == '}') closed++;
+        }
+
+        return true;
+      }
+    }
+
+    /// <summary>
+    /// Finds the sum of all numbers in the provided range that are divisible by three and five.
+    /// </summary>
+    /// <param name="range">The range for the sum.</param>
+    /// <returns>The sum.</returns>
+    public int SumOfMultipleOfThreeOrFive(int range)
+    {
+      return Enumerable.Range(1, range).Where(x => (x % 3 == 0) || (x % 5 == 0)).Sum();
+    }
+  }
+}
---
+++ JobNimbusCodingChallenge/UnitTest1.cs
@@ -0,0 +1,30 @@
+using System;
+using Xunit;
+
+namespace JobNimbusCodingChallenge
+{
+  public class UnitTest1
+  {
+    [Fact]
+    public void BracketsTest()
+    {
+      Challenges challenges = new();
+      Assert.True(challenges.ClosedBrackets(""));
+      Assert.True(challenges.ClosedBrackets("{}"));
+      Assert.True(challenges.ClosedBrackets("{{}}{}"));
+      Assert.True(challenges.ClosedBrackets("Lorem { ipsum dolor sit amet, consectetur adipiscing elit, sed do { eiusmod tempor incididunt ut labore }et dolore magna aliqua.Ut enim ad minim ven} iam,"));
+      Assert.False(challenges.ClosedBrackets("{{}}}{"));
+      Assert.False(challenges.ClosedBrackets("}{"));
+      Assert.False(challenges.ClosedBrackets("{{}"));
+      Assert.False(challenges.ClosedBrackets("{}}"));
+      Assert.False(challenges.ClosedBrackets("Duis{ aute irure dolor }in reprehenderit {in { voluptate} velit esse cillum dolore eu fugiat nulla pariatur."));
+    }
+
+    [Fact]
+    public void SumTest()
+    {
+      Challenges challenges = new();
+      Assert.Equal(233168, challenges.SumOfMultipleOfThreeOrFive(999));
+    }
+  }
+}
